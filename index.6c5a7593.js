c442e005a1b7506edf1e27d5e690e3aa60083470
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-07 19:37:33 +0000

    Deploying to gh-pages from @ Altruist538/goit-js-hw-11@17821d417fc994f8acafc60f5da945d48009e61b 🚀

---
+++ index.6c5a7593.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form").querySelector("#submit"),document.querySelector(".load-more");document.querySelector(".gallery");
+//# sourceMappingURL=index.6c5a7593.js.map
---
+++ index.cd3398ba.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form").querySelector("#submit"),document.querySelector(".load-more"),document.querySelector(".gallery");
+//# sourceMappingURL=index.cd3398ba.js.map
