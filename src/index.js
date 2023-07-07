17821d417fc994f8acafc60f5da945d48009e61b
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-07 22:37:03 +0300

    чистка

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 const searchFormEl = document.querySelector('.search-form');
 const buttonSubmitEl = searchFormEl.querySelector('#submit');
 const buttonLoadMoreEl = document.querySelector('.load-more');
-let galleryEl = document.querySelector('gallery');
+let galleryEl = document.querySelector('.gallery');
