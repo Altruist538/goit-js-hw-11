fc92781ec95c4e91762081449fee84b345cd99c3
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-07-09 19:38:10 +0000

    Deploying to gh-pages from @ Altruist538/goit-js-hw-11@58fbe8d2509903ad8ece44b482b7866780bedcde 🚀

--- index.6c5a7593.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").querySelector("#submit"),document.querySelector(".load-more");document.querySelector(".gallery");
-//# sourceMappingURL=index.6c5a7593.js.map
--- index.cd3398ba.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").querySelector("#submit"),document.querySelector(".load-more"),document.querySelector(".gallery");
-//# sourceMappingURL=index.cd3398ba.js.map
